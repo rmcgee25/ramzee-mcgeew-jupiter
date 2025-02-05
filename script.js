83d71c5c2d4922ad06aafe4e027a835b858156b3
ramzee25 <mcgeeramzee@gmail.com>
2025-02-04 21:41:22 -0500

    added form for messages

--- script.js
+++ script.js
@@ -21,3 +21,35 @@ for (let i=0; i <skills.length ; i++) {
     skillsList.appendChild(skill);
 }
 
+const messageForm = document.getElementsByName('leave_message')[0];
+messageForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let username = document.getElementsByName('usersName')[0].value;
+    let email = document.getElementsByName('usersEmail')[0].value;
+    let text = document.getElementsByName('usersMessage')[0].value;
+
+    console.log(username, email, text);
+
+    const messageSection = document.getElementById('messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+    newMessage.innerHTML = `
+    <a href = "mailto:${email}">${username}</a>
+    <span>${text}</span>
+    `;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function() {
+        const entry = this.parentNode;
+        entry.remove()
+    });
+
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset();
+});
+
